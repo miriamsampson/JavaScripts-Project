36c22bf9257ddc5dc0ec3cf2e1037459d0d31f30
miriamsampson <sumayahsampson@gmail.com>
2025-04-16 03:08:08 -0400

    Functions Assignment

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+// a function that uses the =+ operator
+function myFunction() {
+    var sentence ="I am learning"; //creating the variable
+    sentence =+ " a lot from this course!"; //using the =+ operator to add to the variable
+    document.getElementById("Concatenate").innerHTML = sentence; //using document.getElementId()
+}
